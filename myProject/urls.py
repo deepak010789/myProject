06b434040822c104e4f821cd2cbf0eec5a8afb0d
deepak010789 <deepak010789@gmail.com>
2013-03-13 02:24:49 +0530

    Update urls.py

--- myProject/urls.py
+++ myProject/urls.py
@@ -5,13 +5,5 @@ dajaxice_autodiscover()
 urlpatterns = patterns('',
     (r'^index/$','myProject.Premiere.views.search'),
     (r'^home/$','myProject.Premiere.views.home'),
-#    (r'^home/(?P<url>(?:.*?://)[\w\-\_\.\@\:\/\?\#\=]*)','myProject.Premiere.views.func'),
-#    (r'^home/(?P<url>[/]?[\w]+[/]?([\w]+)?)$','myProject.Premiere.views.func'),
-#    (r'^home/(?P<url>([/][\w]+)+([/][\w]+[:]?[\w|\d]+)+)$','myProject.Premiere.views.func'),
     url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
-    # url(r'^myProject/', include('myProject.foo.urls')),
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
